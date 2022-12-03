66768795ea314411f69aaeb4d2de63ec862a6d45
Damian <d@c64.ch>
2022-12-03 21:17:50 +0100

    End of Part 2

--- src/main.asm
+++ src/main.asm
@@ -1,5 +1,25 @@
 BasicUpstart2(start)
 *=$0810
 start:
-	inc $d020
-	jmp start
+	
+	lda #$2
+	jsr $0fb8
+
+loop:
+
+	lda #$60
+!:	cmp $d012
+	bne !-
+
+	lda #$62
+!:	cmp $d012
+	bne !-
+
+	jsr $0fc5
+
+	jmp loop
+
+
+* = $0e50 "Sid"
+.import c64 "data\sid.prg",0, $500
+
